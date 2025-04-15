54229fd20e3cd4841fed36baebe31f0fbb465f73
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-04-15 16:20:39 -0300

    update ocrutils

--- src/imageops.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-def pictore_or_shotScream(img):
-    print("carro")
---
+++ src/ocrutils/imageops.py
@@ -0,0 +1,7 @@
+import cv2
+
+def is_a_picture(img):
+    print("carro")
+
+def prepare_image(img):
+    return img
