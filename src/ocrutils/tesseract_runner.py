705d411fb966f7db0014029628817a9f94e615ae
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-04-18 11:55:02 -0300

    no use

--- src/ocrutils/tesseract_runner.py
+++ src/ocrutils/tesseract_runner.py
@@ -1,12 +1,11 @@
 from pathlib import Path
-from ocrutils.imageops import is_a_picture, prepare_image
 import subprocess
 import pandas as pd
 import io
 
 
-def img_to_data_dict(config) -> dict:
-    img_path = Path.cwd().joinpath('data','wordsearch.png')  
+def img_to_data_dict(config : str) -> dict:
+    img_path = Path.cwd().joinpath('src','data','wordsearch.png')  
     img_path = str(img_path)
     command = [
         "tesseract",
@@ -15,7 +14,7 @@ def img_to_data_dict(config) -> dict:
         config,
         'tsv'               #retorno não é somente o texto, tem mais informaçãos, similiar a usar image_to_data()
     ]
-    result = subprocess.run(command, cwd= str(Path.cwd().joinpath('data')), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(command, cwd= str(Path.cwd().joinpath('src','data')), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     df = pd.read_csv(io.StringIO(result.stdout), sep="\t")
     output_dict = df.to_dict(orient="list")
